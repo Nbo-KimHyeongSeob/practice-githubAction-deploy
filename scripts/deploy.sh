cd27c6c97e62337d543229cbff65e86ab339b12e
Nbo-KimHyeongSeob <seobseoby7@gmail.com>
2022-08-08 15:52:39 +0900

    배포 자동화 실습 마지막 과정4, DEPLOY_JAR 경로 확인

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,4 +25,5 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
+echo "> DEPLOY_JAR 확인 : $DEPLOY_JAR"  >> /home/ubuntu/action/deploy.log
 sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
